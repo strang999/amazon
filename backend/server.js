9d110b7a5e6071df8954702c56a2308b26141450
Anton Shumeiko <49491454+strang999@users.noreply.github.com>
2020-11-05 12:45:14 +0200

    fixing

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,19 @@
 import express from "express";
 import data from "./data";
+
 const app = express();
 
-app.get("/api/products/:id", (req, res) => {
+app.get("/api/product/:id", (req, res) => {
   const productId = req.params.id;
   const product = data.products.find((x) => x._id === productId);
+  console.log(product);
   if (product) res.send(product);
   else res.status(404).send({ msg: "Product Not Found..." });
 });
-app.get("/api/products/", (req, res) => {
+app.get("/api/products", (req, res) => {
   res.send(data.products);
 });
 
-app.listen(5000, () => {
-  console.log("server started at port 5000");
+app.listen(80, () => {
+  console.log("server started at port 80");
 });
